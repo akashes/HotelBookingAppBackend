e720c751d9662bba7a5936fdd6aa144aa9d34d05
akash <akashes5753279@gmail.com>
2025-09-10 20:49:00 +0530

    tutut

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const { errorMonitor } = require('stream')
 app.use('/uploads',express.static(__dirname+'/uploads')) 
 
 const allowedOrigins = [
+    'https://neon-taffy-87930d.netlify.app',
     'https://imaginative-pothos-581c1a.netlify.app',
     process.env.REACT_APP_DEPLOYED_ORIGIN,
   "https://merry-cranachan-0db122.netlify.app", // deployed frontend
